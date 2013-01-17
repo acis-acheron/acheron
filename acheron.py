0b4cbb8f8f8518b46f9eff854a150f02c4f8a8e0
Benjamin Woodruff <benjaminwoodruff@ufl.edu>
2013-01-17 12:05:29 -0500

    Code cleanup and logging improvements
    Now uses the logging module.

--- acheron.py
+++ acheron.py
@@ -1,40 +1,64 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
 from daemon import Daemon
-import socket, sys
+import socket
+import sys
+import json
+import logging
+
+log = logging.getLogger('acheron')
 
 class Acheron(Daemon):
-    def format_for_styx(self, msg):
+    def __init__(self, *args, **kwargs):
+        Daemon.__init__(self, *args, **kwargs)
+        self.styx_id = 0
+
+    @staticmethod
+    def format_for_styx(msg):
+        msg = json.dumps(msg)
         return msg + ' '*(4095-len(msg)) + '\0'
 
+    def styx_call(self, method, *args):
+        self.styx_socket.sendall(self.format_for_styx({
+            'jsonrpc': '2.0',
+            'method': method,
+            'params': [json.dumps(a) for a in args],
+            'id': self.styx_id,
+        }))
+        self.styx_id += 1
+
     def run(self):
+        log.info('Starting daemon')
         try:
-            self.ipop_listener = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        except socket.error:
-            sys.stderr.write('acheron: error creating IPOP socket\n')
+            self.ipop_listener = socket.socket(socket.AF_INET,
+                                               socket.SOCK_DGRAM)
+        except:
+            log.critical('error creating IPOP socket')
             sys.exit(1)
         self.ipop_listener.bind(('127.0.0.1', 55123))
 
         try:
             self.styx_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        except socket.error:
-            sys.stderr.write('acheron: error creating Styx socket\n')
+        except:
+            log.critical('error creating Styx socket')
             sys.exit(1)
 
         # TODO: parse the strongswan config file for the styx socket path
-        self.styx_socket.connect('/var/run/styx.sock')
+        socket_filename = '/var/run/styx.sock'
+        try:
+            self.styx_socket.connect(socket_filename)
+        except:
+            log.critical('error connecting to styx socket. %s does not exist.' %
+                         socket_filename)
+            sys.exit(1)
 
         while True:
             # alternatively, push to queue, peek, pop when confirmation received
             todo = self.ipop_listener.recv(16).strip()
-            self.styx_socket.sendall(self.format_for_styx('{"jsonrpc":"2.0", "method":"version", "params":["xx"], "id":0}'))
+
+            self.styx_call('version', 'xx')
             result = self.styx_socket.recv(4096)
-            f = open('/var/log/acheron.log', 'a+')
-            print >> f, 'todo = ', todo, 'result = ', result
-            f.flush()
-            f.close()
+            log.info('todo = %s, result = %s', todo, result)
             # send styx message
             # acknowledge reply
-            
-        
-        
