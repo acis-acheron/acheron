05f9727d9de9e512af5498ed84d0b185fbfae949
Thom Dixon <thom@thomdixon.org>
2013-02-28 21:49:51 -0500

    Improve logging. Mimic Vin's call to connect
    We now log when we obtain our IPOP IPv4 address. Moreover, we now
    send similar parameters to Styx as Vin in connect.

--- acheron.py
+++ acheron.py
@@ -19,7 +19,6 @@ log = logging.getLogger('acheron')
 class Acheron(Daemon):
     def __init__(self, *args, **kwargs):
         Daemon.__init__(self, *args, **kwargs)
-        self.styx = Styx(config.styx_unix_socket_path)
 
     @staticmethod
     def get_ip_address(ifname):
@@ -33,12 +32,19 @@ class Acheron(Daemon):
 
     def run(self):
         log.info('starting daemon')
-        try:
-            self.ipop_addr = self.get_ip_address('tapipop')
-        except IOError:
-            log.critical('error finding our IPOP address. Sleeping.')
-            time.sleep(1.0)
-            self.run()
+        self.styx = Styx(config.styx_unix_socket_path)
+        
+        sleep_count = 0
+        while True:
+            try:
+                self.ipop_addr = self.get_ip_address('tapipop')
+                log.info('found IPOP IPv4 address: %s' % self.ipop_addr)
+                break
+            except IOError:
+                if sleep_count % 10 == 0:
+                    log.critical('could not find IPOP IPv4 address. Sleeping.')
+                time.sleep(0.5)
+                sleep_count += 1
         
         try:
             self.ipop_listener = socket.socket(socket.AF_INET,
@@ -51,10 +57,18 @@ class Acheron(Daemon):
         while True:
             # alternatively, push to queue, peek, pop when confirmation received
             addr = self.ipop_listener.recv(16).strip()
+            conn_id = self.styx.message_id
 
             self.styx.addConfig(
-                'conn_%d' % self.styx.message_id,
+                'conn_%d' % conn_id,
                 self.ipop_addr,
                 addr,
+                'leftcert=myCert.pem',
+                'rightid=%any',
+                'leftid=%any',
                 'keyexchange=ikev2'
+            )
+            self.styx.connect(
+                False,
+                'conn_%d' % conn_id
             )
